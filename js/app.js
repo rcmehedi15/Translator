08db8b1b10c4c9d838f439d046a0901d45fdc345
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-03-30 22:01:39 +0600

    some change

--- js/app.js
+++ js/app.js
@@ -4,14 +4,26 @@ const targetText = document.getElementById('targetText');
 const targetLang = document.getElementById('targetLang');
 
 translateBtn.addEventListener('click', () => {
-	const source = sourceText.value;
-	const lang = targetLang.value;
-	const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${encodeURI(source)}`;
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			const translation = data[0][0][0];
-			targetText.value = translation;
-		})
-		.catch(error => console.error(error));
+  const source = sourceText.value.trim();
+  const lang = targetLang.value;
+
+  if (!source) {
+    alert('Please enter a source text.');
+    return;
+  }
+
+  if (lang === 'auto') {
+    alert('Please select a target language.');
+    return;
+  }
+
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${encodeURI(source)}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const translation = data[0][0][0];
+      targetText.value = translation;
+    })
+    .catch(error => console.error(error));
 });
